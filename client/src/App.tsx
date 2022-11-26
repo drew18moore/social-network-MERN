b86d22e31f843395c219c25931277ad482e616eb
Drew <dcm752052@gmail.com>
2022-11-26 17:47:31 -0500

    Installed react-router-dom and created two routes in App.tsx for login and register.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { Link, Route, Routes } from "react-router-dom"
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
 export default function App() {
 
-
   return (
     <div className="App">
-      hello world
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
   )
 }
