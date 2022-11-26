39ebe47b0a05256199ebf585e43fdebdfbf3461d
Drew <dcm752052@gmail.com>
2022-11-26 16:43:53 -0500

    Created auth.js in routes.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 const port = 3000
+const authRouter = require("./routes/auth")
+
 require('dotenv').config()
 
 // Connect to MongoDB database
-const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (err) => {
@@ -13,6 +15,10 @@ db.on('error', (err) => {
 db.once('open', () => {
   console.log("Connected to DB");
 })
+
+// Routing
+app.use(express.json())
+app.use('/api/auth', authRouter)
 
 
 app.listen(port, () => {
