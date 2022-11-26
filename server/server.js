e222d6eeb47092970f21d68fb078a6c4497b7074
Drew <dcm752052@gmail.com>
2022-11-26 16:19:56 -0500

    Installed mongoose and wrote code to connect to db.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express()
 const port = 3000
+require('dotenv').config()
+
+// Connect to MongoDB database
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (err) => {
+  console.log(err);
+})
+db.once('open', () => {
+  console.log("Connected to DB");
+})
+
 
 app.listen(port, () => {
   console.log(`Node server listening on port ${port}`);
