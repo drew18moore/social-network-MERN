7659b1bb642e7dd0545af11b8108eb233ebe4654
Drew <dcm752052@gmail.com>
2022-11-26 18:11:40 -0500

    Created a form inside Login.tsx.

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function Login() {
+
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log(usernameRef.current.value, passwordRef.current.value);
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="input-username">Username</label>
+      <input ref={usernameRef} type="text" name="username" id="input-username" required />
+
+      <label htmlFor='input-password'>Password</label>
+      <input ref={passwordRef} type="password" name="password" id="input-password" required />
+
+      <button type='submit'>Log In</button>
+    </form>
   )
 }
