3b47351f3432dafd22ccd827e088bd9f4c088018
Drew <dcm752052@gmail.com>
2022-11-26 17:21:29 -0500

    Created model for User. Implemented register.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const router = express.Router()
+const User = require("../models/User")
 
-router.post("/register", (req, res) => {
-  res.send("Register")
+router.post("/register", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+  try {
+    const newUser = await user.save()
+    res.status(200).json(newUser)
+  } catch (err) {
+    console.log(err);
+  }
 })
 
 router.post("/login", (req, res) => {
