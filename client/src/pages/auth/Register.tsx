abf4ceae151123ca1b945d3d85f6babd2340454a
Drew <dcm752052@gmail.com>
2022-11-26 20:18:37 -0500

    Implemented signup form.

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Axios from 'axios'
 
 export default function Register() {
+
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const passwordConfirmRef = useRef<HTMLInputElement>(null)
+
+  const [error, setError] = useState<string>("")
+
+  async function register(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setError("")
+    if (usernameRef.current !== null && passwordRef.current !== null && passwordConfirmRef.current !== null) {
+      if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+        return setError("Passwords do not match")
+      }
+      await Axios.post("http://localhost:3000/api/auth/register", {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value,
+        passwordConfirm: passwordConfirmRef.current.value
+      }).then((res) => {
+        console.log(res.data);
+      }).catch((err) => {
+        setError(err.response.data.message);
+      })
+    } else {
+      console.error("usernameRef.current, passwordRef.current, or passwordConfirmRef is null")
+    }
+  }
+
   return (
-    <div>Register</div>
+    <>
+      {error}
+      <form onSubmit={register}>
+        <label htmlFor="input-username">Username</label>
+        <input ref={usernameRef} type="text" name="username" id="input-username" required />
+
+        <label htmlFor='input-password'>Password</label>
+        <input ref={passwordRef} type="password" name="password" id="input-password" required />
+
+        <label htmlFor='input-password-confirm'>Confirm Password</label>
+        <input ref={passwordConfirmRef} type="password" name='password-confirm' id='input-password-confirm' required />
+
+        <button type='submit'>Sign Up</button>
+      </form>
+    </>
+    
   )
 }
