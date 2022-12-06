e9a2e0363b5a35e3d30311749b918a4bc5e24fa8
Drew <dcm752052@gmail.com>
2022-12-06 18:23:13 -0500

    Created a card for profile page that displays profile picture and username.

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import "./profile.css"
+import { useAuth } from '../../contexts/AuthContext'
 
 export default function Profile() {
+  const { currentUser } = useAuth()
   return (
     <>
       <Navbar />
-      Profile
+      <div className="profile-container">
+        <div className='profile-card'>
+          <div className='profile-img-username'>
+            <img className='profile-picture' src='/default-pfp.jpg' alt='profile picture' />
+            <div className="profile-name-username">
+              <h1 className='name'>{currentUser.username}</h1>
+              <h2 className='username'>@{currentUser.username}</h2>
+            </div>
+            
+          </div>
+          
+        </div>
+      </div>
     </>
   )
 }
