0243ddfb8fd919a97e98d830c9da110cb69682a1
Drew <dcm752052@gmail.com>
2022-12-05 23:41:01 -0500

    Formatted the dates

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import "./post.css"
 
 export default function Post({ username, postBody, createdAt }) {
+  let date = new Date(createdAt)
+  const dateOptions = {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  } 
   return (
     <div className='post'>
       <div className="post-header">
         <p className='post-username'>{username}</p>
-        <p className='post-date'>{createdAt}</p>
+        <p className='post-date'>{date.toLocaleString("en-US", dateOptions)}</p>
       </div>
       <p className='post-body'>{postBody}</p>
     </div>
