6ce6cb1eaf38fb7e9e1cf7e5437c02c35756295a
Drew <dcm752052@gmail.com>
2022-12-08 19:06:37 -0500

    Added img element to post component. Added some divs and changed styling to align img.

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -10,22 +10,35 @@ export default function Post({ fullname, username, postBody, createdAt }) {
   };
   return (
     <div className="post">
-      <div className="post-header">
-        <div className="left-post-header">
-          <p className="post-fullname">{fullname}</p>
-          <p className="post-username">@{username}</p>
-          <p className="post-dot">&#8226;</p>
-          <p className="post-date">
-            {date.toLocaleString("en-US", dateOptions)}
-          </p>
+      <div className="post-content">
+        <div className="post-picture">
+          <img src="/default-pfp.jpg" alt="" />
         </div>
-        <div className="right-post-header">
-          <div className="meatball-btn">
-            <span class="material-symbols-outlined">more_horiz</span>
+        <div className="post-text">
+          <div className="post-header">
+            <div className="left-post-header">
+              <p className="post-fullname">{fullname}</p>
+              <p className="post-username">@{username}</p>
+              <p className="post-dot">&#8226;</p>
+              <p className="post-date">
+                {date.toLocaleString("en-US", dateOptions)}
+              </p>
+            </div>
+            <div className="right-post-header">
+              <div className="meatball-btn">
+                <span class="material-symbols-outlined">more_horiz</span>
+              </div>
+            </div>
           </div>
+          <p className="post-body">{postBody}</p>
+          
+          {/* <div className="like-comment-share-btns">
+          <span class="material-symbols-rounded">thumb_up</span>
+          <span class="material-symbols-rounded">chat_bubble</span>
+          <span class="material-symbols-rounded">google_plus_reshare</span>
+        </div> */}
         </div>
       </div>
-      <p className="post-body">{postBody}</p>
       <hr />
       <div className="like-comment-share-btns">
         <span class="material-symbols-rounded">thumb_up</span>
