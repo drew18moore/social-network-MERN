849ac456778f28946175587cd9daa1de86d9e659
Drew <dcm752052@gmail.com>
2022-12-08 21:08:15 -0500

    Added material icons for PostDropdown list items.

--- client/src/components/dropdown/PostDropdown.jsx
+++ client/src/components/dropdown/PostDropdown.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
 import { useAuth } from "../../contexts/AuthContext";
 
 export default function PostDropdown({ username }) {
   const { currentUser } = useAuth();
-  
+
   return (
     <ul>
-        {username === currentUser.username && <li>Edit</li>}
-        {username === currentUser.username && <li>Delete</li>}
-        {username !== currentUser.username && <li>Report</li>}
-      </ul>
-  )
+      {username === currentUser.username && (
+        <li>
+          <span class="material-symbols-outlined">edit</span>
+          <p>Edit</p>
+        </li>
+      )}
+      {username === currentUser.username && (
+        <li>
+          <span class="material-symbols-rounded">delete</span>
+          <p>Delete</p>
+        </li>
+      )}
+      {username !== currentUser.username && (
+        <li>
+          <span class="material-symbols-rounded">report</span>
+          <p>Report</p>
+        </li>
+      )}
+    </ul>
+  );
 }
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -15,7 +15,6 @@ export default function Post({ fullname, username, postBody, createdAt }) {
   };
 
   const openDropdown = () => {
-    console.log("object");
     setShowdropdown(prev => !prev)
   }
 
