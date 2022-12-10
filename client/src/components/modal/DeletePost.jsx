9e617d34d9dc14a62ad78f7f837662af773d9de8
Drew <dcm752052@gmail.com>
2022-12-09 19:14:58 -0500

    Styled the DeletePost component.

--- client/src/components/modal/DeletePost.jsx
+++ client/src/components/modal/DeletePost.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 export default function DeletePost() {
   return (
     <div>
-      <h1>Are you sure you want to delete this post?</h1>
-      <button>Cancel</button>
-      <button>Delete</button>
+      <h1 className='modal-centered'>Delete Post?</h1>
+      <hr />
+      <h4 className='modal-centered'>This action can't be undone</h4>
+      <div className="modal-btns">
+        <button className='cancel-btn'>Cancel</button>
+        <button className='delete-btn'>Delete</button>
+      </div>
     </div>
   )
 }
