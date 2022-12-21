051c86b32884af0964f19a8e8d1114c850e61183
Drew <dcm752052@gmail.com>
2022-12-20 20:53:20 -0500

    A user's own posts now appear on their profile page.

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -8,6 +8,7 @@ import ChangeProfilePicture from "../../components/modal/ChangeProfilePicture";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Post from "../../components/post/Post";
 
 export default function Profile() {
   const { username } = useParams();
@@ -19,16 +20,28 @@ export default function Profile() {
   const [isFollowing, setIsFollowing] = useState();
   const [followBtnText, setFollowBtnText] = useState("Following");
 
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    axios
-      .get(`http://192.168.1.2:3000/api/users/${username}`)
-      .then((res) => {
-        setUser(res.data);
-        setIsFollowing(() => res.data.followers.includes(currentUser._id));
-      })
-      .catch((err) => {
-        console.log("Error", err);
-      });
+    const fetchData = async () => {
+      // Fetch user data
+      await axios
+        .get(`http://192.168.1.2:3000/api/users/${username}`)
+        .then((res) => {
+          setUser(res.data);
+          setIsFollowing(() => res.data.followers.includes(currentUser._id));
+        })
+        .catch((err) => {
+          console.log("Error", err);
+        });
+      // Fetch user posts
+      await axios
+        .get(`http://192.168.1.2:3000/api/posts/${username}`)
+        .then((res) => {
+          setPosts(res.data);
+        });
+    };
+    fetchData();
   }, [username]);
 
   const followUser = () => {
@@ -43,6 +56,21 @@ export default function Profile() {
       .catch((err) => {
         console.log("Error", err);
       });
+  };
+
+  const deletePostById = (postId) => {
+    const indexToDelete = posts.findIndex((x) => x._id === postId);
+    console.log(indexToDelete);
+    let updatedPosts = [...posts];
+    updatedPosts.splice(indexToDelete, 1);
+    setPosts(updatedPosts);
+  };
+
+  const editPost = (post) => {
+    const indexToUpdate = posts.findIndex((x) => x._id === post._id);
+    let updatedPosts = [...posts];
+    updatedPosts[indexToUpdate].postBody = post.postBody;
+    setPosts(updatedPosts);
   };
 
   return (
@@ -96,9 +124,37 @@ export default function Profile() {
             )}
           </div>
           <div className="bottom">
-            <span className="following"><span className="count">{user.following && user.following.length}</span> Following</span>
-            <span className="followers"><span className="count">{user.followers && user.followers.length}</span> Followers</span>
+            <span className="following">
+              <span className="count">
+                {user.following && user.following.length}
+              </span>{" "}
+              Following
+            </span>
+            <span className="followers">
+              <span className="count">
+                {user.followers && user.followers.length}
+              </span>{" "}
+              Followers
+            </span>
           </div>
+        </div>
+        <h2 className="posts-heading">Posts</h2>
+        <div className="posts">
+          {posts.map((post) => (
+            <Post
+              key={post._id}
+              postId={post._id}
+              fullname={post.fullname}
+              username={post.username}
+              postBody={post.postBody}
+              createdAt={post.createdAt}
+              profilePicture={post.profilePicture}
+              deletePostById={deletePostById}
+              editPost={editPost}
+              isLiked={post.likes.includes(currentUser._id)}
+              numLikes={post.likes.length}
+            />
+          ))}
         </div>
       </div>
       {showEditProfileModal && (
