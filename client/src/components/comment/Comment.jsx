63aa27c9e9375c2c9db8d97b4aa3669db6c84657
Drew <dcm752052@gmail.com>
2022-12-27 21:47:55 -0500

    Added meatball button to comments.

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import "./comment.css"
+import React from "react";
+import "./comment.css";
+import { useState } from "react";
+import Dropdown from "../dropdown/Dropdown";
+import PostDropdown from "../dropdown/PostDropdown";
 
 export default function Comment({ fullname, profilePicture, commentBody }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
-    <div className='comment'>
+    <div className="comment">
       <div className="comment-pfp">
         <img src={profilePicture} alt="" />
       </div>
       <div className="comment-text">
-        <p className="fullname">{fullname}</p>
+        <div className="comment-top">
+          <p className="fullname">{fullname}</p>
+          <div className="comment-dropdown-wrapper">
+            <div className="meatball-btn" onClick={() => setShowDropdown(true)}>
+              <span className="material-symbols-outlined">more_horiz</span>
+            </div>
+            {showDropdown && (
+              <Dropdown setShowDropdown={setShowDropdown}>
+                <PostDropdown />
+              </Dropdown>
+            )}
+          </div>
+        </div>
+
         <p className="comment-body">{commentBody}</p>
       </div>
     </div>
-  )
+  );
 }
