79a6ef3f5c4fc0c43c7c0c6265b282c0b22e06a1
Drew <dcm752052@gmail.com>
2022-12-27 19:05:40 -0500

    Finished styling for CommentModal.

--- client/src/components/modal/CommentModal.jsx
+++ client/src/components/modal/CommentModal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function CommentModal({
   postId,
@@ -6,25 +7,46 @@ export default function CommentModal({
   username,
   postBody,
   profilePicture,
-  date
+  date,
 }) {
+  const { currentUser } = useAuth();
+
   return (
     <div className="comment-modal">
-      <h1 className="modal-centered">Write a Comment</h1>
+      <h1 className="modal-centered">Reply</h1>
       <hr />
-      <div className="modal-post">
-        <div className="modal-post-picture">
-          <img src={profilePicture} alt="" />
-        </div>
-        <div className="modal-post-text">
-          <div className="modal-post-header">
-            <div className="modal-post-fullname">{fullname}</div>
-            <p className="modal-post-username">@{username}</p>
-            <p className="modal-post-dot">&#8226;</p>
-            <p className="modal-post-date">{date}</p>
+      <div className="comment-modal-main">
+        <div className="modal-post">
+          <div className="modal-post-picture">
+            <img src={profilePicture} alt="" />
           </div>
-          <p className="modal-post-body">{postBody}</p>
+          <div className="modal-post-text">
+            <div className="modal-post-header">
+              <div className="modal-post-fullname">{fullname}</div>
+              <p className="modal-post-username">@{username}</p>
+              <p className="modal-post-dot">&#8226;</p>
+              <p className="modal-post-date">{date}</p>
+            </div>
+            <p className="modal-post-body">{postBody}</p>
+          </div>
         </div>
+        <div className="modal-post-spacer">
+          <div className="vertical-line"></div>
+        </div>
+        <form className="modal-comment-form">
+          <div className="input-area">
+            <div className="modal-comment-picture">
+              <img src={currentUser.img} alt="" />
+            </div>
+            <textarea
+              id="post-body-input"
+              placeholder="Write your reply"
+            ></textarea>
+          </div>
+          <button type="submit" id="post-btn">
+            Reply
+          </button>
+        </form>
       </div>
     </div>
   );
