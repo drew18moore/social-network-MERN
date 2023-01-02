6421ffc41feec2325aac12058b7daaf75a9f8fff
Drew <dcm752052@gmail.com>
2023-01-01 20:38:40 -0500

    Added bcrypt to dependencies. User passwords are now hashed with bcrypt.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // CREATE account
 router.post("/register", async (req, res) => {
-  const user = new User({
-    fullname: req.body.fullname,
-    username: req.body.username,
-    password: req.body.password,
-  });
-
   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      fullname: req.body.fullname,
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
     const newUser = await user.save();
     res.status(200).json(newUser);
   } catch (err) {
@@ -25,10 +27,13 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({
       username: req.body.username,
-      password: req.body.password,
+      // password: req.body.password,
     });
 
     !user && res.status(404).json({ message: "User not found" });
+    if(!await bcrypt.compare(req.body.password, user.password)) {
+      res.status(400).json({ message: "Incorrect password" })
+    }
 
     let profilePicture;
     if (user.img.data) {
