294ae18e11bf22dea428b3144e9aa5288220ba2a
Drew <dcm752052@gmail.com>
2023-01-07 20:45:54 -0500

    Changed BrowserRouter to HashRouter to potentially fix the 404 errors with Vercel.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import { AuthProvider } from "./contexts/AuthContext"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </AuthProvider>
   </React.StrictMode>
 )
