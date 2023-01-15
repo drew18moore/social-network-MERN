b661397ded24e2d3293ed6087edc2c27921b6956
Drew <dcm752052@gmail.com>
2023-01-15 17:28:14 -0500

    Added back button to settings page. Added an appearance and delete account section to settings.

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./settings.css"
 
 const Settings = () => {
+  const navigate = useNavigate();
   return (
     <>
-      
+      <div className="settings-top">
+        <div className="back-btn" onClick={() => navigate("/")}>
+          <span className="material-symbols-outlined">arrow_back</span>
+        </div>
+        <p>Settings</p>
+      </div>
+      <div className="settings-container">
+        <div className="appearance">
+          <h2>Appearance</h2>
+        </div>
+        <div className="delete-account">
+          <h2>Delete Account</h2>
+          <hr />
+          <p>Once you delete your account, there is no going back. Please be certain.</p>
+          <button>Delete your account</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
