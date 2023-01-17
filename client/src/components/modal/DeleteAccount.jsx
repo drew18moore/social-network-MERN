dd36a89079e5ca790e311e5ba67cf8304250e35f
Drew <dcm752052@gmail.com>
2023-01-16 23:09:06 -0500

    Modified user delete route to accept password to check if it is valid before deletion. Implemented handleSubmit function in DeleteAccount component. Added error message to DeleteAccount modal.

--- client/src/components/modal/DeleteAccount.jsx
+++ client/src/components/modal/DeleteAccount.jsx
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import api from "../../api/api";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const CONFIRMATION_PHRASE = "delete my account";
 
 const DeleteAccount = () => {
   const [confirmationPhrase, setConfirmationPhrase] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const { currentUser } = useAuth();
+  const navigate = useNavigate();
 
   const validate = () => {
-    return confirmationPhrase === CONFIRMATION_PHRASE && confirmPassword.length > 0
-  }
+    return (
+      confirmationPhrase === CONFIRMATION_PHRASE && confirmPassword.length > 0
+    );
+  };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const response = await api.delete(`api/users/delete/${currentUser._id}`, {
+        data: { password: confirmPassword },
+      });
+
+      console.log(response.data);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data.message);
+      console.log(err);
+    }
+  };
   return (
     <div className="delete-account-modal">
       <h1 className="modal-centered">Delete Account</h1>
       <hr />
-      <p className="warning-text">You are about to <strong>permanently delete your account</strong>. This action <strong>cannot be undone</strong>.</p>
+      <p className="warning-text">
+        You are about to <strong>permanently delete your account</strong>. This
+        action <strong>cannot be undone</strong>.
+      </p>
+      {error && <p className="error-message">{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           <p>
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,6 +7,7 @@ const Image = require("../models/Image");
 const fs = require("fs");
 const { deletePost } = require("../routes/posts");
 const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -244,19 +245,24 @@ router.get("/:username/followers", async (req, res) => {
 // DELETE user by id
 router.delete("/delete/:userId", async (req, res) => {
   try {
+    const user = await User.findById(req.params.userId);
+
+    if (!(await bcrypt.compare(req.body.password, user.password))) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
     const postIds = await Post.find({ userId: req.params.userId })
-    .select("_id")
-    .then((res) => res.map((val) => val._id));
-  for (let id of postIds) {
-    await deletePost(id, req.params.userId)
-  }
+      .select("_id")
+      .then((res) => res.map((val) => val._id));
+    for (let id of postIds) {
+      await deletePost(id, req.params.userId);
+    }
 
-  const user = await User.findById(req.params.userId)
-  await user.deleteOne()
+    await user.deleteOne();
 
-  res.status(200).json({ message: "User account has been deleted" })
+    res.status(200).json({ message: "User account has been deleted" });
   } catch (err) {
-    res.status(500).json({ message: err })
+    res.status(500).json({ message: err });
   }
 });
 
