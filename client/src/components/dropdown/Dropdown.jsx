13043877c598108483ca7e4a0edcac897eed7492
Drew <dcm752052@gmail.com>
2023-01-22 22:39:24 -0500

    Dropdown component will now appear above the trigger element if there isn't enough space below. Removed arrow ::after element.

--- client/src/components/dropdown/Dropdown.jsx
+++ client/src/components/dropdown/Dropdown.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useRef, useState, useLayoutEffect } from "react";
 import "./dropdown.css";
 
 export default function Dropdown({ children, setShowDropdown }) {
+  const dropdownRef = useRef();
+  const [dropdownStyle, setDropdownStyle] = useState({});
+
+  useLayoutEffect(() => {
+    const rect = dropdownRef.current.getBoundingClientRect();
+    if (rect.bottom > window.innerHeight) {
+      console.log("object");
+      setDropdownStyle({
+        bottom: "100%",
+      });
+    } else {
+      setDropdownStyle({ 
+        top: "100%" 
+      });
+    }
+    console.log(dropdownStyle);
+  }, []);
+
   return (
     <>
-      <div className="dropdown">{children}</div>
+      <div ref={dropdownRef} style={dropdownStyle} className="dropdown">
+        <span className="arrow"></span>
+        {children}
+      </div>
       <div
         className="dropdown-backdrop"
         onClick={(e) => {
           e.stopPropagation();
-          setShowDropdown(prev => !prev)
+          setShowDropdown((prev) => !prev);
         }}
       ></div>
     </>
