b916523978ceac31714b2c1b45b1b0f07da056d6
Drew <dcm752052@gmail.com>
2023-01-25 18:50:55 -0500

    Created a usersController. Moved logic from routes/users.js to usersController.js. Moved multer middleware function to its own file: middleware/multer.js

---
+++ server/controllers/usersController.js
@@ -0,0 +1,259 @@
+const User = require("../models/User");
+const fs = require("fs");
+const { deletePost } = require("../routes/posts");
+const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
+
+const changeProfilePicture = async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    return res
+      .status(403)
+      .json({ message: "You can only change your own profile picture" });
+  }
+
+  const updates = {
+    img: {
+      data: fs.readFileSync("images/" + req.file.filename),
+      contentType: "image/png",
+    },
+  };
+
+  User.findByIdAndUpdate(
+    req.body.userId,
+    updates,
+    { new: true },
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: err });
+      } else {
+        let profilePicture;
+        const buffer = Buffer.from(result.img.data);
+        const b64String = buffer.toString("base64");
+        profilePicture = `data:image/png;base64,${b64String}`;
+        return res.status(200).json(profilePicture);
+      }
+    }
+  );
+};
+
+const editUser = async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    return res
+      .status(403)
+      .json({ message: "You can only update your own account" });
+  }
+
+  const user = await User.findById(req.params.id);
+
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(400).json({ message: "Incorrect password" });
+  }
+
+  const updates = {
+    fullname: req.body.fullname,
+    username: req.body.username,
+  };
+
+  User.findByIdAndUpdate(
+    req.body.userId,
+    updates,
+    { new: true },
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: err });
+      } else {
+        let profilePicture;
+        const buffer = Buffer.from(result.img.data);
+        const b64String = buffer.toString("base64");
+        profilePicture = `data:image/png;base64,${b64String}`;
+        const user = {
+          ...result.toJSON(),
+          img: profilePicture,
+        };
+        return res.status(200).json(user);
+      }
+    }
+  );
+};
+
+const getUserByUsername = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+
+    let profilePicture;
+    if (user.img.data) {
+      const buffer = Buffer.from(user.img.data);
+      const b64String = buffer.toString("base64");
+      profilePicture = `data:image/png;base64,${b64String}`;
+    } else {
+      profilePicture = "/default-pfp.jpg";
+    }
+
+    const mainUser = {
+      ...user.toJSON(),
+      img: profilePicture,
+    };
+
+    res.status(200).json(mainUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const followUser = async (req, res) => {
+  if (req.params.username === req.body.currUsername) {
+    return res.status(403).json({ message: "You cannot follow yourself" });
+  }
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const currUser = await User.findOne({ username: req.body.currUsername });
+    if (!user.followers.includes(currUser._id)) {
+      await user.updateOne({ $push: { followers: currUser._id.toString() } });
+      await currUser.updateOne({ $push: { following: user._id.toString() } });
+      res.status(200).json("User has been followed");
+    } else {
+      await user.updateOne({ $pull: { followers: currUser._id.toString() } });
+      await currUser.updateOne({ $pull: { following: user._id.toString() } });
+      res.status(200).json("User has been unfollowed");
+    }
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+const getUnfollowedUsers = async (req, res) => {
+  try {
+    const users = await User.find({
+      followers: { $nin: req.params.id },
+      _id: { $nin: req.params.id },
+    });
+
+    const unfollowedUsers = users.map((user) => {
+      let profilePicture;
+      if (user.img.data) {
+        const buffer = Buffer.from(user.img.data);
+        const b64String = buffer.toString("base64");
+        profilePicture = `data:image/png;base64,${b64String}`;
+      } else {
+        profilePicture = "/default-pfp.jpg";
+      }
+
+      return {
+        ...user.toJSON(),
+        img: profilePicture,
+      };
+    });
+
+    res.status(200).json(unfollowedUsers);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getFollowedUsers = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.params.username,
+    });
+    const users = await User.find({
+      followers: user._id.toString(),
+    });
+
+    const following = users.map((user) => {
+      let profilePicture;
+      if (user.img.data) {
+        const buffer = Buffer.from(user.img.data);
+        const b64String = buffer.toString("base64");
+        profilePicture = `data:image/png;base64,${b64String}`;
+      } else {
+        profilePicture = "/default-pfp.jpg";
+      }
+
+      return {
+        ...user.toJSON(),
+        img: profilePicture,
+      };
+    });
+
+    res.status(200).json({
+      user: {
+        fullname: user.fullname,
+        username: user.username,
+      },
+      following: following,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getFollowers = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.params.username,
+    });
+    const users = await User.find({
+      following: user._id.toString(),
+    });
+
+    const followers = users.map((user) => {
+      let profilePicture;
+      if (user.img.data) {
+        const buffer = Buffer.from(user.img.data);
+        const b64String = buffer.toString("base64");
+        profilePicture = `data:image/png;base64,${b64String}`;
+      } else {
+        profilePicture = "/default-pfp.jpg";
+      }
+
+      return {
+        ...user.toJSON(),
+        img: profilePicture,
+      };
+    });
+
+    res.status(200).json({
+      user: {
+        fullname: user.fullname,
+        username: user.username,
+      },
+      followers: followers,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+
+    if (!(await bcrypt.compare(req.body.password, user.password))) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const postIds = await Post.find({ userId: req.params.userId })
+      .select("_id")
+      .then((res) => res.map((val) => val._id));
+    for (let id of postIds) {
+      await deletePost(id, req.params.userId);
+    }
+
+    await user.deleteOne();
+
+    res.status(200).json({ message: "User account has been deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+module.exports = {
+  changeProfilePicture,
+  editUser,
+  getUserByUsername,
+  followUser,
+  getUnfollowedUsers,
+  getFollowedUsers,
+  getFollowers,
+  deleteUser,
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,275 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
-const multer = require("multer");
-const path = require("path");
-const Image = require("../models/Image");
-const fs = require("fs");
-const { deletePost } = require("../routes/posts");
-const Post = require("../models/Post");
-const bcrypt = require("bcrypt");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
+const usersController = require("../controllers/usersController");
+const multer = require("../middleware/multer")
 
 // Change profile picture
-router.put("/change-img/:id", upload.single("image"), async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    return res
-      .status(403)
-      .json({ message: "You can only change your own profile picture" });
-  }
-
-  const updates = {
-    img: {
-      data: fs.readFileSync("images/" + req.file.filename),
-      contentType: "image/png",
-    },
-  };
-
-  User.findByIdAndUpdate(
-    req.body.userId,
-    updates,
-    { new: true },
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({ message: err });
-      } else {
-        let profilePicture;
-        const buffer = Buffer.from(result.img.data);
-        const b64String = buffer.toString("base64");
-        profilePicture = `data:image/png;base64,${b64String}`;
-        return res.status(200).json(profilePicture);
-      }
-    }
-  );
-});
+router.put(
+  "/change-img/:id",
+  multer.upload.single("image"),
+  usersController.changeProfilePicture
+);
 
 // Edit user info
-router.put("/:id", async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    return res
-      .status(403)
-      .json({ message: "You can only update your own account" });
-  }
-
-  const user = await User.findById(req.params.id);
-
-  if (!(await bcrypt.compare(req.body.password, user.password))) {
-    return res.status(400).json({ message: "Incorrect password" });
-  }
-
-  const updates = {
-    fullname: req.body.fullname,
-    username: req.body.username,
-  };
-
-  User.findByIdAndUpdate(
-    req.body.userId,
-    updates,
-    { new: true },
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({ message: err });
-      } else {
-        let profilePicture;
-        const buffer = Buffer.from(result.img.data);
-        const b64String = buffer.toString("base64");
-        profilePicture = `data:image/png;base64,${b64String}`;
-        const user = {
-          ...result.toJSON(),
-          img: profilePicture,
-        };
-        return res.status(200).json(user);
-      }
-    }
-  );
-});
+router.put("/:id", usersController.editUser);
 
 // Get user by unique username
-router.get("/:username", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.params.username });
-
-    let profilePicture;
-    if (user.img.data) {
-      const buffer = Buffer.from(user.img.data);
-      const b64String = buffer.toString("base64");
-      profilePicture = `data:image/png;base64,${b64String}`;
-    } else {
-      profilePicture = "/default-pfp.jpg";
-    }
-
-    const mainUser = {
-      ...user.toJSON(),
-      img: profilePicture,
-    };
-
-    res.status(200).json(mainUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/:username", usersController.getUserByUsername);
 
 // Follow user
-router.put("/follow/:username", async (req, res) => {
-  if (req.params.username === req.body.currUsername) {
-    return res.status(403).json({ message: "You cannot follow yourself" });
-  }
-  try {
-    const user = await User.findOne({ username: req.params.username });
-    const currUser = await User.findOne({ username: req.body.currUsername });
-    if (!user.followers.includes(currUser._id)) {
-      await user.updateOne({ $push: { followers: currUser._id.toString() } });
-      await currUser.updateOne({ $push: { following: user._id.toString() } });
-      res.status(200).json("User has been followed");
-    } else {
-      await user.updateOne({ $pull: { followers: currUser._id.toString() } });
-      await currUser.updateOne({ $pull: { following: user._id.toString() } });
-      res.status(200).json("User has been unfollowed");
-    }
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.put("/follow/:username", usersController.followUser);
 
 // Get all unfollowed users
-router.get("/all-unfollowed/:id", async (req, res) => {
-  try {
-    const users = await User.find({
-      followers: { $nin: req.params.id },
-      _id: { $nin: req.params.id },
-    });
-
-    const unfollowedUsers = users.map((user) => {
-      let profilePicture;
-      if (user.img.data) {
-        const buffer = Buffer.from(user.img.data);
-        const b64String = buffer.toString("base64");
-        profilePicture = `data:image/png;base64,${b64String}`;
-      } else {
-        profilePicture = "/default-pfp.jpg";
-      }
-
-      return {
-        ...user.toJSON(),
-        img: profilePicture,
-      };
-    });
-
-    res.status(200).json(unfollowedUsers);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/all-unfollowed/:id", usersController.getUnfollowedUsers);
 
 // Get all followed users
-router.get("/:username/following", async (req, res) => {
-  try {
-    const user = await User.findOne({
-      username: req.params.username,
-    });
-    const users = await User.find({
-      followers: user._id.toString(),
-    });
-
-    const following = users.map((user) => {
-      let profilePicture;
-      if (user.img.data) {
-        const buffer = Buffer.from(user.img.data);
-        const b64String = buffer.toString("base64");
-        profilePicture = `data:image/png;base64,${b64String}`;
-      } else {
-        profilePicture = "/default-pfp.jpg";
-      }
-
-      return {
-        ...user.toJSON(),
-        img: profilePicture,
-      };
-    });
-
-    res.status(200).json({
-      user: {
-        fullname: user.fullname,
-        username: user.username,
-      },
-      following: following,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/:username/following", usersController.getFollowedUsers);
 
 // Get all followers
-router.get("/:username/followers", async (req, res) => {
-  try {
-    const user = await User.findOne({
-      username: req.params.username,
-    });
-    const users = await User.find({
-      following: user._id.toString(),
-    });
-
-    const followers = users.map((user) => {
-      let profilePicture;
-      if (user.img.data) {
-        const buffer = Buffer.from(user.img.data);
-        const b64String = buffer.toString("base64");
-        profilePicture = `data:image/png;base64,${b64String}`;
-      } else {
-        profilePicture = "/default-pfp.jpg";
-      }
-
-      return {
-        ...user.toJSON(),
-        img: profilePicture,
-      };
-    });
-
-    res.status(200).json({
-      user: {
-        fullname: user.fullname,
-        username: user.username,
-      },
-      followers: followers,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/:username/followers", usersController.getFollowers);
 
 // DELETE user by id
-router.delete("/delete/:userId", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-
-    if (!(await bcrypt.compare(req.body.password, user.password))) {
-      return res.status(400).json({ message: "Incorrect password" });
-    }
-
-    const postIds = await Post.find({ userId: req.params.userId })
-      .select("_id")
-      .then((res) => res.map((val) => val._id));
-    for (let id of postIds) {
-      await deletePost(id, req.params.userId);
-    }
-
-    await user.deleteOne();
-
-    res.status(200).json({ message: "User account has been deleted" });
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.delete("/delete/:userId", usersController.deleteUser);
 
 module.exports = router;
