ccac330f4bd97f6e95160bf5aef89b94f3bffbc0
Drew <dcm752052@gmail.com>
2023-02-03 22:14:30 -0500

    A single page of users are displayed when Connect.jsx loads.

--- client/src/pages/connect/Connect.jsx
+++ client/src/pages/connect/Connect.jsx
@@ -1,8 +1,32 @@
+import { createRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "../../api/api";
+import User from "../../components/user/User";
+import { useAuth } from "../../contexts/AuthContext";
 import "./connect.css";
 
 const Connect = () => {
+  const { currentUser } = useAuth();
+  const [unfollowedUsers, setUnfollowedUsers] = useState([]);
+  const limit = 5;
   const navigate = useNavigate();
+
+  const fetchData = async () => {
+    try {
+      const response = await api.get(
+        `/api/users/all-unfollowed/${currentUser._id}?page=1&limit=${limit}`
+      );
+      setUnfollowedUsers((prev) => [...prev, ...response.data.unfollowedUsers])
+      console.log(response.data)
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="connect-top">
@@ -12,6 +36,9 @@ const Connect = () => {
         <p>Connect</p>
       </div>
       <h1 className="connect-h1">People you may know</h1>
+      <div className="unfollowed-users">
+        {unfollowedUsers && unfollowedUsers.map((user) => <User user={user} key={user._id}/>)}
+      </div>
     </>
   );
 };
