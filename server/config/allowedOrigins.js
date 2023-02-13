3bea21834ed6d067dcc3ef25de42a36d06d8f9b0
Drew <dcm752052@gmail.com>
2023-02-13 18:31:48 -0500

    Added new domain to allowedDomains.

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:5173"];
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:5173", "https://social-network-mern.vercel.app/"];
 
 module.exports = allowedOrigins;
