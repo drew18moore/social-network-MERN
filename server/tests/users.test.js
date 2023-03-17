ac89ad8686bf12c9f07c3b6f9e0a2cb84b4e8a18
Drew <dcm752052@gmail.com>
2023-03-17 19:30:18 -0400

    Added to test to check if db has correct updated data.

--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
+const User = require("../models/User");
 const { connect, disconnect, reset } = require("./config/database");
 
 beforeAll(async () => {
@@ -15,35 +16,37 @@ afterAll(async () => {
 });
 
 describe("PUT /users/:id", () => {
-  describe("On success...", () => {
-    test("return 200 status code", async () => {
-      const userData = {
-        fullname: "test fullname",
-        username: "testusername",
-        password: "password123",
-      };
-      const registeredUser = await request(app)
-        .post("/api/auth/register")
-        .send(userData);
-      expect(registeredUser.statusCode).toBe(200);
-      const updates = {
-        userId: registeredUser.body._id,
-        fullname: "new fullname",
-        username: "newusername",
-        bio: "new bio",
-        password: userData.password,
-      }
-      const updatedUser = await request(app)
-        .put(`/api/users/${registeredUser.body._id}`)
-        .send(updates)
-        .set("Authorization", `Bearer ${registeredUser.body.accessToken}`);
-      expect(updatedUser.statusCode).toBe(200);
-      const expectedResponseBody = {
-        fullname: updates.fullname,
-        username: updates.username,
-        bio: updates.bio,
-      }
-      expect(updatedUser.body).toEqual(expectedResponseBody)
-    })
-  })
-})
+  test("On success, return 200 status code, return correct JSON data, and store correct data in DB", async () => {
+    const userData = {
+      fullname: "test fullname",
+      username: "testusername",
+      password: "password123",
+    };
+    const registeredUser = await request(app)
+      .post("/api/auth/register")
+      .send(userData);
+    expect(registeredUser.statusCode).toBe(200);
+    const updates = {
+      userId: registeredUser.body._id,
+      fullname: "new fullname",
+      username: "newusername",
+      bio: "new bio",
+      password: userData.password,
+    };
+    const updatedUser = await request(app)
+      .put(`/api/users/${registeredUser.body._id}`)
+      .send(updates)
+      .set("Authorization", `Bearer ${registeredUser.body.accessToken}`);
+    expect(updatedUser.statusCode).toBe(200);
+    const expectedResponseBody = {
+      fullname: updates.fullname,
+      username: updates.username,
+      bio: updates.bio,
+    };
+    expect(updatedUser.body).toEqual(expectedResponseBody);
+    const user = await User.findById(registeredUser.body._id);
+    expect(user.fullname).toEqual(updates.fullname);
+    expect(user.username).toEqual(updates.username);
+    expect(user.bio).toEqual(updates.bio);
+  });
+});
