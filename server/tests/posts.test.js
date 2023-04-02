4861fa0075ff8a13757ef1e8d897d7452597f07a
Drew <dcm752052@gmail.com>
2023-04-01 23:35:10 -0400

    Wrote test for like post endpoint testing that a user's id will be correctly removed from post's likes if previously liked.

--- server/tests/posts.test.js
+++ server/tests/posts.test.js
@@ -458,7 +458,7 @@ describe("DELETE /posts/:id", () => {
 
 describe("PUT /posts/:id/like", () => {
   describe("On success return 200 status code and...", () => {
-    test("Should like post if previously unliked", async () => {
+    test("Should ADD user's id to post's likes if previously unliked", async () => {
       // Register user
       const userData1 = {
         fullname: "test fullname",
@@ -484,6 +484,39 @@ describe("PUT /posts/:id/like", () => {
       expect(likePost.statusCode).toBe(200)
       post = await Post.findById(newPost.body._id)
       expect(post.likes).toContain(registeredUser.body._id)
+    });
+    test("Should REMOVE user's id to post's likes if previously liked", async () => {
+      // Register user
+      const userData1 = {
+        fullname: "test fullname",
+        username: "testusername1",
+        password: "password123",
+      };
+      const registeredUser = await request(app)
+        .post("/api/auth/register")
+        .send(userData1);
+      expect(registeredUser.statusCode).toBe(200);
+      // New post
+      const postBody = "Post 1";
+      const newPost = await request(app)
+        .post("/api/posts/new")
+        .send({ postBody: postBody })
+        .set("Authorization", `Bearer ${registeredUser.body.accessToken}`);
+      expect(newPost.statusCode).toBe(200);
+      // Like post
+      const likePost = await request(app)
+        .put(`/api/posts/${newPost.body._id}/like`)
+        .set("Authorization", `Bearer ${registeredUser.body.accessToken}`);
+      expect(likePost.statusCode).toBe(200)
+      let post = await Post.findById(newPost.body._id)
+      expect(post.likes).toContain(registeredUser.body._id)
+      // Unlike post
+      const unlikePost = await request(app)
+        .put(`/api/posts/${newPost.body._id}/like`)
+        .set("Authorization", `Bearer ${registeredUser.body.accessToken}`);
+      expect(unlikePost.statusCode).toBe(200)
+      post = await Post.findById(newPost.body._id)
+      expect(post.likes).not.toContain(registeredUser.body._id)
     });
   });
 });
