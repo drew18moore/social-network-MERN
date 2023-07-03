f8360a34cf6a991e4a0e3cf4b6f3242d557ab542
Drew <dcm752052@gmail.com>
2023-07-02 21:44:23 -0400

    Added toast notification for when post url is copied to clipboard.

--- client/src/components/dropdown/ShareDropdown.jsx
+++ client/src/components/dropdown/ShareDropdown.jsx
@@ -1,18 +1,29 @@
 import { BiCopy } from "react-icons/bi";
+import { toast } from "react-hot-toast";
+import { useTheme } from "../../contexts/ThemeContext";
 
 const ShareDropdown = ({ setShowDropdown, postId, authorUsername }) => {
+  const { theme } = useTheme();
   const textToCopy = `${window.location.href}${authorUsername}/posts/${postId}`;
   return (
     <ul className="share-dropdown">
-      <li onClick={(e) => {
-        e.stopPropagation();
-        navigator.clipboard.writeText(textToCopy);
-        setShowDropdown(prev => !prev);
-      }}>
+      <li
+        onClick={(e) => {
+          e.stopPropagation();
+          navigator.clipboard.writeText(textToCopy);
+          setShowDropdown((prev) => !prev);
+          toast.success("Copied to clipboard", {
+            style: {
+              backgroundColor: `${theme === "light" ? "" : "#16181c"}`,
+              color: `${theme === "light" ? "" : "#fff"}`,
+            },
+          });
+        }}
+      >
         <BiCopy />
-        <p>Copy link to post</p> 
+        <p>Copy link to post</p>
       </li>
     </ul>
-  )
-}
-export default ShareDropdown
+  );
+};
+export default ShareDropdown;
