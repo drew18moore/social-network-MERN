a92872c5359eeb846af2777dc4f6ca74b5742b65
Drew <dcm752052@gmail.com>
2023-07-11 19:51:20 -0400

    Added skeleton loading to WhoToFollow component.

--- client/src/components/whoToFollow/WhoToFollow.jsx
+++ client/src/components/whoToFollow/WhoToFollow.jsx
@@ -3,12 +3,14 @@ import { useNavigate } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { useAuth } from "../../contexts/AuthContext";
 import User from "../user/User";
+import { UserSkeleton } from "../loading/SkeletonLoading";
 import "./whoToFollow.css";
 
 const WhoToFollow = () => {
   const { currentUser } = useAuth();
   const axiosPrivate = useAxiosPrivate();
   const [unfollowedUsers, setUnfollowedUsers] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const limit = 4;
   const [isNextPage, setIsNextPage] = useState();
   const navigate = useNavigate();
@@ -16,6 +18,7 @@ const WhoToFollow = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const response = await axiosPrivate.get(
           `/api/users/all-unfollowed/${currentUser._id}?page=1&limit=${limit}`
         );
@@ -23,10 +26,21 @@ const WhoToFollow = () => {
         response.data.numFound < limit ? setIsNextPage(false) : setIsNextPage(true);
       } catch (err) {
         console.error(err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
   }, []);
+
+  const skeletons = () => {
+    let arr = [];
+    for (let i = 0; i < 4; i++) {
+      arr.push(<UserSkeleton key={i} />);
+    }
+
+    return <>{arr}</>;
+  };
 
   return (
     <div className="who-to-follow">
@@ -35,6 +49,7 @@ const WhoToFollow = () => {
         unfollowedUsers.map((user) => {
           return <User user={user} key={user._id} />;
         })}
+      {isLoading && <div className="whotofollow-skeleton-container">{skeletons()}</div>}
       {isNextPage && <button className="show-more" onClick={() => navigate("/connect")}>Show More</button>}
     </div>
   );
