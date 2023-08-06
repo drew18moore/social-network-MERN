488d6c992673ecf50a65eccc64d6f6cec55fcb54
Drew <dcm752052@gmail.com>
2023-08-05 22:00:20 -0500

    Removed any types from Modal.tsx

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -1,26 +1,32 @@
-import React, { useEffect } from "react";
+import React, { ReactNode, useEffect } from "react";
 import "./modal.css";
 import { MdOutlineClose } from "react-icons/md";
 
-export default function Modal({ children, setShowModal }: any) {
-
+type Props = {
+  children: ReactNode;
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export default function Modal({ children, setShowModal }: Props) {
   useEffect(() => {
-    document.body.style.overflow = "hidden"
+    document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = "visible"
-    }
-  }, [])
+      document.body.style.overflow = "visible";
+    };
+  }, []);
 
   return (
     <div
       className="modal-backdrop"
       onClick={(e) => {
         e.stopPropagation();
-        setShowModal((prev: any) => !prev)
+        setShowModal((prev) => !prev);
       }}
     >
       <div className="modal-container" onClick={(e) => e.stopPropagation()}>
-        <button className="close-modal-btn" onClick={() => setShowModal((prev: any) => !prev)}>
+        <button
+          className="close-modal-btn"
+          onClick={() => setShowModal((prev) => !prev)}
+        >
           <MdOutlineClose size="1.5rem" />
         </button>
         {children}
