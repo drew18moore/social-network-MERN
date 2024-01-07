82e973ca3588cff7b8e87865e2209d1ef33850b2
Drew <dcm752052@gmail.com>
2024-01-06 21:17:42 -0500

    Moved login logic into useLogin hook.

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,52 +1,27 @@
-import React, { useRef, useState } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import api from "../../api/api";
-import { useAuth } from "../../contexts/AuthContext";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 import LoadingAnimation from "../../components/loading/LoadingAnimation";
-import { AxiosError } from "axios";
+import useLogin from "../../hooks/auth/useLogin";
 
 export default function Login() {
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+  const { login, isLoading, error } = useLogin();
 
-  const { setCurrentUser } = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
-
-  async function login(e: React.FormEvent<HTMLFormElement>) {
+  async function onLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    setError("");
-    setIsLoading(true);
-    try {
-      const response = await api.post(
-        "/api/auth/login",
-        {
-          username: usernameRef.current?.value.trim().toLowerCase(),
-          password: passwordRef.current?.value,
-        },
-        {
-          withCredentials: true,
-        }
-      );
-      const accessToken = response?.data?.accessToken;
-      setCurrentUser(response.data);
-      setIsLoading(false);
-      navigate(from, { replace: true });
-    } catch (err: any) {
-      setIsLoading(false);
-      console.error(err);
-      setError(err.response.data.message);
-    }
+
+    await login({
+      username: usernameRef.current?.value.trim().toLowerCase()!,
+      password: passwordRef.current?.value!,
+    });
   }
 
   return (
     <>
       <div className="auth-container">
-        <form onSubmit={login} className="auth-form">
+        <form onSubmit={onLogin} className="auth-form">
           <h2 className="auth-heading">Log In</h2>
           {error ? <p className="error-message">{error}</p> : ""}
           <input
